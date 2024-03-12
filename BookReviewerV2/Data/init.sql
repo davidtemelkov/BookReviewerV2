32647198108e2b04f2e9fba54c6023ef689779fe
davidtemelkov <dntemelkov@gmail.com>
2024-03-12 17:43:33 +0200

    Changed default sqllite db to a mysql docker container

---
+++ BookReviewerV2/Data/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS BookReviewerV2;
+
+USE BookReviewerV2;
--- BookReviewerV2/Program.cs
+++ BookReviewerV2/Program.cs
@@ -8,10 +8,18 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
                        throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 3, 0))));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.Password.RequireDigit = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequiredLength = 0;
+    })
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
